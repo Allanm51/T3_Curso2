da37489000e542ea17695fa3cfdd08dedcda1902
Allanm51 <allan.santos@bsatech.io>
2025-10-17 14:40:44 -0300

    unidade 4

--- Alura.Adopet.Console/Comandos/Import.cs
+++ Alura.Adopet.Console/Comandos/Import.cs
@@ -30,12 +30,10 @@ namespace Alura.Adopet.Console.Comandos
             {
                 List<Pet> listaDePet = leitor.RealizaLeitura();
                 foreach (var pet in listaDePet)
-                {
-                   System.Console.WriteLine(pet);                  
+                {                       
                    await clientPet.CreatePetAsync(pet);               
                 }
-                System.Console.WriteLine("Importação concluída!");
-                return Result.Ok().WithSuccess(new SuccessWithPets(listaDePet));
+                return Result.Ok().WithSuccess(new SuccessWithPets(listaDePet,"Importação Realizada com Sucesso!"));
             }
             catch (Exception exception)
             {
